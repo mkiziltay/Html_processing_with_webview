3b20c3900c03cc73283a17211aa2fd525da66c37
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-04-07 23:38:30 +0300

    Update MainActivity.java

--- app/src/main/java/com/retrofit/htmlprocessing/MainActivity.java
+++ app/src/main/java/com/retrofit/htmlprocessing/MainActivity.java
@@ -14,7 +14,6 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
