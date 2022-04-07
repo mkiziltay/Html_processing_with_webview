5b1899716db0cf6aac786cba5fd8957240630132
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-04-07 23:39:57 +0300

    Update BarChartActivity.java

--- app/src/main/java/com/retrofit/htmlprocessing/BarChartActivity.java
+++ app/src/main/java/com/retrofit/htmlprocessing/BarChartActivity.java
@@ -1,16 +1,13 @@
 package com.retrofit.htmlprocessing;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
-
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
-
 import java.util.ArrayList;
 
 public class BarChartActivity extends AppCompatActivity {
